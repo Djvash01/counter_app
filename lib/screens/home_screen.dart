7bc7abc7a99b5aaca4bb10cb371417fb322b3796
David Yepes <djvash01@gmail.com>
2022-06-27 16:51:01 -0500

    Add column center and labels

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('hola desde home screen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Couter app'),
+        elevation: 10,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Clicks counter'),
+            Text('0'),
+          ],
+        ),
+      )
+    );
   }
 }
