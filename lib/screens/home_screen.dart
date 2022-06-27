936dfca33c470d1dbfc65c91125281c77b28aee6
David Yepes <djvash01@gmail.com>
2022-06-27 16:20:04 -0500

    Create scaffold with body

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('hola desde home screen'));
+    return const Scaffold(body: Center(child: Text('hola desde home screen')));
   }
 }
